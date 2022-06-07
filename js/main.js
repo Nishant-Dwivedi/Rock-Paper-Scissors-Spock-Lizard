44a3610d81061e586dbe5b65320cc4303d6ca873
Nishant Dwivedi <nishant.dwivedi@hotmail.com>
2022-06-07 13:31:38 +0530

    created the basic layout

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// player makes a selection via the dom
+//  computer makes a selection after a delay
+//  compute() figures out who the winner is
+// score() updates the score after each round and displays it
+
+
+// let playerSelection = prompt("Enter your selection: ");
+// let computerSelection = prompt("Enter computer's selection: ");
+
+// const winsAgainst = {
+// rock : ["lizard", "scissors"],
+// paper : ["rock", "spock"],
+// scissors : ["paper", "lizard"],
+// lizard : ["spock", "paper"],
+// spock : ["scissors", "rock"],
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     if (winsAgainst[`${playerSelection}`][i] == `${computerSelection}`){
+//         console.log('YOU WIN!!');
+//         break;
+//     }
+//     else {
+//         console.log('loser!!');
+//     }
+// }
